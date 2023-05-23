e4eec7a2bb04ad07af608db32acddca9825e718e
Auston Youngblood <95832900+austonyb@users.noreply.github.com>
2023-05-22 22:50:43 -0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Auston Youngblood on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
